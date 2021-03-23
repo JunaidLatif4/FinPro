3fc6b1e18035ea23549cd9869742a0559346b533
Serge Amouzou <sergeamouzou@serges-mbp.lan>
2021-03-23 16:13:40 -0700

    reverting price

--- src/views/Pricing/Pricing.js
+++ src/views/Pricing/Pricing.js
@@ -197,9 +197,9 @@ function Pricing() {
 								<div className='plan-card-heading'>
 									<h4>INTRO PLAN</h4>
 									<h1 className='plan-price'>
-										<span>$</span>119.88
+										<span>$</span>14.99
 									</h1>
-									<span>/year</span>
+									<span>/month</span>
 								</div>
 
 								<div className='items-check-list'>
