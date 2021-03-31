80ce39686d43db9a08efdf08333f6af1823e71b9
Serge Amouzou <sergeamouzou@serges-mbp.lan>
2021-03-30 21:44:13 -0700

    Adding Firebase SDK

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+var firebaseConfig = {
+    apiKey: "AIzaSyCpFwZnD_9t6LWsn1V4IDUDjW-7rkcKSiM",
+    authDomain: "finmod-webapp.firebaseapp.com",
+    projectId: "finmod-webapp",
+    storageBucket: "finmod-webapp.appspot.com",
+    messagingSenderId: "1054130185159",
+    appId: "1:1054130185159:web:0ba92095c2818726fbefe2",
+    measurementId: "G-E3YJGTYJJB"
+  };
+
+	// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
 	<React.StrictMode>
 		<Router>
