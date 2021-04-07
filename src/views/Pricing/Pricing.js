42833e90c2033db054b21e406c4434198a126a44
Serge Amouzou <sergeamouzou@serges-mbp.lan>
2021-04-07 09:04:26 -0700

    Updating price to 9.99

--- src/views/Pricing/Pricing.js
+++ src/views/Pricing/Pricing.js
@@ -335,7 +335,7 @@ function Pricing() {
 								<div className='plan-card-heading'>
 									<h4>INTRO PLAN</h4>
 									<h1 className='plan-price'>
-										<span>$</span>1.00
+										<span>$</span>9.99
 									</h1>
 									<span>/month</span>
 								</div>
